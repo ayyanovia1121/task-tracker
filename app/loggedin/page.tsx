7dd6d773e977a3095247012d19c8e6c5a20f924d
ayyanovia1121 <noviayya1121@gmail.com>
2024-09-13 16:49:25 +0700

    Loggin Environment

---
+++ app/loggedin/page.tsx
@@ -0,0 +1,13 @@
+// import { Button } from "@/components/ui/button";
+import Tasks from "@/components/compacts/tasks";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Haloow</h1>
+      <Tasks />
+
+      
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { signInAction } from "@/actions/auth-action";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Haloow</h1>
-      <Button variant="default">Hello</Button>
+      <h1>Login</h1>
+
+      <form action={signInAction}>
+        <Button variant="default">Log in</Button>
+      </form>
     </main>
   );
 }
